227133b22f0b1291b9121595d77455fc527cb794
Julien Athomas <julienathomas@Juliens-MacBook-Pro.local>
2017-03-12 09:37:04 -0700

    update

--- src/userControllers/UserControllers_test.go
+++ src/userControllers/UserControllers_test.go
@@ -1,11 +1,111 @@
 package userControllers
 
-import "testing"
+import ("testing"
+        "models"
+        "mydb")
 
-func TestFirstTest(t *testing.T) {
-  a := 10
-  b := 9
-  if (a != b) {
-    t.Error("values are different", a, b)
+func TestEmptyUserModelForCreation(t *testing.T) {
+  var user models.UserModel
+
+  valid, err := checkUserInformationsForCreation(user)
+  if (valid == true) {
+    t.Error("Check should return false: ", err)
+  }
+}
+
+func TestEmptyFieldUserModelForCreation(t *testing.T) {
+  var user models.UserModel
+
+  user.Firstname = ""
+  user.LastName = ""
+  user.Password = ""
+  user.Email = ""
+  valid, err := checkUserInformationsForCreation(user)
+  if (valid == true) {
+    t.Error("Check should return false: ", err)
+  }
+}
+
+func TestValidUserCreationWithPassword(t *testing.T) {
+  database.Init()
+  var user models.UserModel
+
+  user.Firstname = "Julien"
+  user.LastName = "Athomas"
+  user.Password = "1234"
+  user.Email = "julien.athomas45@epitech.eu"
+  valid, err := checkUserInformationsForCreation(user)
+  if (valid == false) {
+    t.Error("Check should return false: ", err)
+  }
+}
+
+func TestCreationWithExistingEmail(t *testing.T) {
+  database.Init()
+  var user models.UserModel
+
+  user.Firstname = "Julien"
+  user.LastName = "Athomas"
+  user.Password = "1234"
+  user.Email = "julien.athomas@epitech.eu"
+  valid, err := checkUserInformationsForCreation(user)
+  if (valid == true) {
+    t.Error("Check should return false: ", err)
+  }
+}
+
+func TestCreationWithoutEmail(t *testing.T) {
+  database.Init()
+  var user models.UserModel
+
+  user.Firstname = "Julien"
+  user.LastName = "Athomas"
+  user.Password = "1234"
+  user.Email = ""
+  valid, err := checkUserInformationsForCreation(user)
+  if (valid == true) {
+    t.Error("Check should return false: ", err)
+  }
+}
+
+func TestCreationWithoutPassword(t *testing.T) {
+  database.Init()
+  var user models.UserModel
+
+  user.Firstname = "Julien"
+  user.LastName = "Athomas"
+  user.Password = ""
+  user.Email = "julien.athomas@epitech.eu"
+  valid, err := checkUserInformationsForCreation(user)
+  if (valid == true) {
+    t.Error("Check should return false: ", err)
+  }
+}
+
+func TestCreationWithoutFirstname(t *testing.T) {
+  database.Init()
+  var user models.UserModel
+
+  user.Firstname = ""
+  user.LastName = "Athomas"
+  user.Password = "1234"
+  user.Email = "julien.athomas@epitech.eu"
+  valid, err := checkUserInformationsForCreation(user)
+  if (valid == true) {
+    t.Error("Check should return false: ", err)
+  }
+}
+
+func TestCreationWithoutLastname(t *testing.T) {
+  database.Init()
+  var user models.UserModel
+
+  user.Firstname = "Julien"
+  user.LastName = ""
+  user.Password = "1234"
+  user.Email = "julien.athomas@epitech.eu"
+  valid, err := checkUserInformationsForCreation(user)
+  if (valid == true) {
+    t.Error("Check should return false: ", err)
   }
 }
